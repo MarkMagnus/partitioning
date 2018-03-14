9af7541b7a5fa52ae10472780fec9db20c2d1c6f
mark.magnus@impactdata.com.au <mark.magnus@impactdata.com.au>
2018-03-14 14:57:01 +1100

    add search page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -20,4 +20,21 @@ class AccountsController < ApplicationController
   def index
     @accounts = Account.all
   end
+
+  def search()
+    if request.post?
+      name = params['name'] ? "%" + params['name'] + "%" : "%"
+      email = params['email'] ? "%" + params['email'] + "%" : "%"
+      type = params['type'] ? params['type'].to_i : 0
+
+      @contacts = Contact.where("fields->>'name' like ?", name)
+                      .where("fields->>'email' like ?", email)
+                      .where("contact_type = ? or 0 = ?", type, type)
+                      .limit(100)
+      # .includes(:accounts)
+    else
+      @contacts = nil
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Partitioning::Application.routes.draw do
 
+  get :search, to: 'accounts#search'
+  post :search, to: 'accounts#search'
+
   resources :accounts do
     resources :contacts
   end
